ebcc0b3673b5fd89048a2f3a7b43614b20350c8a
jSdCool <jSdCoolgaming@gmail.com>
2025-10-07 22:12:18 -0400

    update to MC 1.21.10 Changed image based captcha to use the new Text sprites instead of the overridden item model\s. item model overrides have been removed from the resource pack generator (old packs should still work)

--- src/main/java/org/cbigames/captcha/CaptchaTask.java
+++ src/main/java/org/cbigames/captcha/CaptchaTask.java
@@ -1,31 +1,26 @@
 package org.cbigames.captcha;
 
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.CustomModelDataComponent;
 import net.minecraft.dialog.AfterAction;
 import net.minecraft.dialog.DialogActionButtonData;
 import net.minecraft.dialog.DialogButtonData;
 import net.minecraft.dialog.DialogCommonData;
 import net.minecraft.dialog.action.DynamicCustomDialogAction;
 import net.minecraft.dialog.body.DialogBody;
-import net.minecraft.dialog.body.ItemDialogBody;
 import net.minecraft.dialog.body.PlainMessageDialogBody;
 import net.minecraft.dialog.input.TextInputControl;
 import net.minecraft.dialog.type.DialogInput;
 import net.minecraft.dialog.type.NoticeDialog;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.common.ShowDialogS2CPacket;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.network.ServerPlayerConfigurationTask;
 import net.minecraft.text.*;
+import net.minecraft.text.object.AtlasTextObjectContents;
 import net.minecraft.util.Identifier;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
 
@@ -81,20 +76,16 @@ public class CaptchaTask implements ServerPlayerConfigurationTask {
         List<DialogBody> body = new ArrayList<>();
         body.add(new PlainMessageDialogBody(Text.of("Enter the text below to prove your not a robot"),300));
 
-        @SuppressWarnings("MismatchedQueryAndUpdateOfCollection") ArrayList<Boolean> flags = new ArrayList<>();
-        @SuppressWarnings("MismatchedQueryAndUpdateOfCollection") ArrayList<String> strings = new ArrayList<>();
-        @SuppressWarnings("MismatchedQueryAndUpdateOfCollection") ArrayList<Integer> colors = new ArrayList<>();
-
         switch (Config.getConfig().getMethod()) {
             case TEXT -> body.add(new PlainMessageDialogBody(captchaText, 300));
             case IMAGE -> {
+                MutableText images = Text.literal(".");
+                images.setStyle(images.getStyle().withBold(true));
                 for(int i:imageNumbers){
-                    ArrayList<Float> number = new ArrayList<>();
-                    number.add((float)i);
-                    ItemStack item = Items.COMMAND_BLOCK_MINECART.getDefaultStack();
-                    item.set(DataComponentTypes.CUSTOM_MODEL_DATA, new CustomModelDataComponent(number,flags,strings,colors));
-                    body.add(new ItemDialogBody(item,Optional.empty(),false,false,16,16));
+                      images.append(Text.object(new AtlasTextObjectContents(AtlasTextObjectContents.DEFAULT_ATLAS,Identifier.of("captcha","item/img"+i))));
+
                 }
+                body.add(new PlainMessageDialogBody(images,300));
                 body.add(new PlainMessageDialogBody(Text.of("note: sometimes these numbers may not be accurate"),300));
             }
         }
